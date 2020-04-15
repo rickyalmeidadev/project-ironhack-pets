0e7848a8d0ea2cef7384aa29b853b806c359f365
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-14 23:25:41 -0300

    add views missing tags and add some indentation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,43 +6,36 @@ const Pet = require('../models/Pet');
 router.get('/', (req, res, next) => {
   if (req.user) {
     const user = req.user;
-    const id = req.user._id
-    
+    const id = req.user._id;
+
     Pet.find({ owner: id })
-    .then(pets => {
-      Event.find()
-      .populate('owner')
-      .then(events => {
-        const petsIds = pets.map(pet => JSON.stringify(pet._id));
-        // console.log(events)
-        // let results = petsIds
-        events = events.filter(event =>{
-        // console.log(typeof event.owner._id)
-        // console.log('=======>', typeof petsIds[0])
-        return petsIds.includes(JSON.stringify(event.owner._id))
+      .then(pets => {
+        Event.find()
+          .populate('owner')
+          .then(events => {
+            const petsIds = pets.map(pet => JSON.stringify(pet._id));
+            events = events.filter(event => {
+              return petsIds.includes(JSON.stringify(event.owner._id));
+            });
 
+            console.log(events);
+            const obj = {
+              user,
+              pets,
+              events,
+            };
+            res.render('user', { obj });
+          })
+          .catch(() => {
+            const obj = {
+              user,
+              pets,
+            };
+            res.render('user', { obj });
+          });
       })
-      
-      console.log(events);
-        const obj = {
-          user,
-          pets,
-          events
-        }
-        res.render('user', { obj });
-      })
-      .catch(() => {
-        const obj = {
-          user,
-          pets
-        }
-        res.render('user', { obj })
+      .catch(error => res.render('user', { user }));
+  }
+});
 
-        }); 
-
-      })
-      .catch(error => res.render('user', { user }))
-    }
-  });
-
-  module.exports = router;
+module.exports = router;
