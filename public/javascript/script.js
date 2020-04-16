1b5d2c1f4daee02f4693bfdb96aa9d9034282217
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-16 00:48:59 -0300

    fix cursor pointer for icons, add hover to edit icons, fill photo label with input value

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,16 +1,25 @@
-console.log('linked');
-
 const info1 = document.getElementById('info-1');
 const info2 = document.getElementById('info-2');
 const info3 = document.getElementById('info-3');
 
-if (window.screen.availWidth > 600) {
-  [info1, info2, info3].forEach(info => {
-    info.classList.add('info-opacity')
-  })
-  window.addEventListener('scroll', () => {
-    if (window.screen.availWidth > 600) if (window.scrollY > 109) info1.classList.add('go-right');
-    if (window.scrollY > 412) info2.classList.add('go-left');
-    if (window.scrollY > 722) info3.classList.add('go-right');
+if (info1) {
+  if (window.screen.width > 600) {
+    [info1, info2, info3].forEach(info => {
+      info.classList.add('info-opacity');
+    });
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 109) info1.classList.add('go-right');
+      if (window.scrollY > 412) info2.classList.add('go-left');
+      if (window.scrollY > 722) info3.classList.add('go-right');
+    });
+  }
+}
+
+const photoInput = document.getElementById('photo-input');
+const photoLabel = document.getElementById('photo-label');
+
+if (photoInput) {
+  photoInput.addEventListener('change', () => {
+    photoLabel.innerHTML = photoInput.value;
   });
 }
