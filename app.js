11d0a817368f99e8ee5b8a67edca68174fe49cc5
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-22 12:13:59 -0300

    change google calback url

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: 'https://pet-schedule.herokuapp.com/auth/google/callback',
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'https://pet-schedule.herokuapp.com/auth/google/callback',
+      // callbackURL: '/auth/google/callback',
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
